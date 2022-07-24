43ea072affe9dfb4263dccd9978ce5afd92811e4
Robin Klöckner <mail@robinkloeckner.com>
2022-07-24 12:01:21 +0200

    Initial commit

---
+++ src/components/LocaleSelect.vue
@@ -0,0 +1,15 @@
+<template>
+  <label for="locale">Locale: </label>
+  <select v-model="$i18n.locale" id="locale">
+    <option v-for="locale in $i18n.availableLocales" :value="locale">{{ locale }}</option>
+  </select>
+</template>
+
+<style scoped>
+  label, select {
+    width: 100%;
+  }
+  select {
+    height: 25px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import App from "./App.vue";
+
+import "./assets/main.css";
+import messages from "@intlify/unplugin-vue-i18n/messages";
+
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: "en",
+  fallbackLocale: "en",
+  availableLocales: ["en", "de"],
+  messages: messages,
+});
+
+const app = createApp(App)
+
+app.use(i18n)
+app.mount('#app')
